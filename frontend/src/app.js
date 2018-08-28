3168c8dc4de49e3aac1e9491e30befb1a1e11207
youngje jo <siosio3103@gmail.com>
2018-08-28 13:26:09 +0000

    Add global-styles.js

--- frontend/src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-
-const MOUNT_NODE = document.getElementById('app');
-const render = () => {
-	ReactDOM.render(<App/>, MOUNT_NODE);
-}
-	
--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+// const AppWrapper = ``
+
 
 class App extends Component {
   render() {
