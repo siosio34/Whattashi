ff3d0cc2301d50abd07b5171d8e54ac086d6cc71
youngje jo <siosio3103@gmail.com>
2018-08-30 14:36:08 +0000

    Add configureStore.js

--- frontend/src/configureStore.js
+++ frontend/src/configureStore.js
@@ -1,1 +1,32 @@
 import { routerMiddleware } from 'react-router-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import createReducer from './reducers';
+
+const sagaMiddleware = createSagaMiddleware();
+
+export default function configureStore(initialState = {}, history) {
+	const middlewares = [sagaMiddleware, routerMiddleware(history)];
+	const enhancers = [applyMiddleware(...middlewares)];
+	
+	const composeEnhancers =
+    process.env.NODE_ENV !== 'production' &&
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+          // TODO: Try to remove when `react-router-redux` is out of beta, LOCATION_CHANGE should not be fired more than once after hot reloading
+          // Prevent recomputing reducers for `replaceReducer`
+          shouldHotReload: false,
+        })
+      : compose;
+  /* eslint-enable */
+
+  const store = createStore(
+    createReducer(),
+    composeEnhancers(...enhancers)
+  );
+  store.runSaga = sagaMiddleware.run;
+	return store;
+	
+}
+
