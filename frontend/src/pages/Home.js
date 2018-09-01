141838c714198bcc90bfdcd651ec4156740ea78b
youngje jo <siosio3103@gmail.com>
2018-09-01 10:49:41 +0000

    Add Login and Home Page

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+	render() {
+		return (
+			<div>홈입니다.</div>
+		)
+	}
+}
+
+export default Home;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {default as LoginForm} from './login/Form';
+
+class Login extends Component{
+	render() {
+		return (
+			<LoginForm />
+		)
+	}
+}
+
+export default Login;
