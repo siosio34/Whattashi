b3f8ea6f2dcd601c59b0ec60e5de591240204462
youngje jo <siosio3103@gmail.com>
2018-09-01 10:50:03 +0000

    Fix don't render frontpage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import { Router } from 'react-router';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './containers/App';
+import configureStore from './configureStore';
+import history from './history';
+import './global-styles';
+
+const initialState ={};
+const store = configureStore(initialState);
+const MOUNT_NODE = document.getElementById('root');
+
+const render = () => {
+	ReactDOM.render(
+		<Provider store={store}>
+			<Router history={history}>
+				<App />
+			</Router>
+		</Provider>,
+		MOUNT_NODE,
+	);
+};
+
+render();
+
+
+
 
